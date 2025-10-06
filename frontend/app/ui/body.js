7bf2a28462b3eccb2005d9b3ca1a91987819d622
Simon Martens <simon.martens@mailbox.org>
2025-10-06 14:20:20 +0200

    Small changes

--- frontend/app/ui/body.js
+++ frontend/app/ui/body.js
@@ -3,9 +3,7 @@ const html = require("choo/html");
 module.exports = function body(main) {
   return function (state, emit) {
     const b = html`
-      <main
-        class="flex flex-col items-center font-sans md:h-screen md:bg-grey-10 dark:bg-black w-full"
-      >
+      <main class="flex flex-col items-center font-sans md:h-screen w-full">
         ${main(state, emit)}
       </main>
     `;
